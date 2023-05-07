0702dd5fda9a4d9b98a0ffac62fff3dded29bb0e
Tarea <ashlycampos00@aragon.unam.mx>
2023-05-07 17:15:27 -0600

    se agrega el proyecto

---
+++ src/main/java/com/mycompany/tarea10poo/Main.java
@@ -0,0 +1,12 @@
+
+
+package com.mycompany.tarea10poo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        VentanaSwing ventanaSwing = new VentanaSwing("Centigrados a Farenheit");
+        
+        
+    }
+}
